ecdceaaddf81b5ab9e8937dfaad207869872f175
Alisha Kelkar <akelkar@email.unc.edu>
2023-04-27 21:04:03 -0400

    rpsls changes

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,23 +1,5 @@
 #!/usr/bin/env node
 import { rpsls } from "../lib/rpsls.js"
-rpsls();
-// import minimist from 'minimist';
-
-
-//     // import minimist from 'minimist';
-//     const args = minimist(process.argv.slice(2));
-
-// if(args.h){
-//     console.log(`
-//     Usage: node-rps [SHOT] 
-//     Play Rock Paper Scissors (RPS)
-//   -h, --help      display this help message and exit
-//   -r, --rules     display the rules and exit
-
-// Examples:
-//   node-rps        Return JSON with single player RPS result.
-//                   e.g. {"player":"rock"}
-//   node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-//                   e.g {"player":"rock","opponent":"scissors","result":"win"}
-//                   `)
-// }
+import minimist  from "minimist";
+const args = minimist(process.argv.slice(2));
+rpsls(args);
