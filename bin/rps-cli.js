7794312a7043663bd449551e8f6228a0e4951f5a
Alisha Kelkar <akelkar@email.unc.edu>
2023-04-27 21:16:58 -0400

    whats wrong

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -2,5 +2,32 @@
 import { rps } from "../lib/rpsls.js"
 import minimist from 'minimist'
 const args = minimist(process.argv.slice(2));
-console.log(rps(args))
+
+if(args.h){
+    console.log(`
+    Usage: node-rps [SHOT] 
+    Play Rock Paper Scissors (RPS)
+-h, --help      display this help message and exit
+-r, --rules     display the rules and exit
+
+Examples:
+node-rps        Return JSON with single player RPS result.
+                e.g. {"player":"rock"}
+node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                e.g {"player":"rock","opponent":"scissors","result":"win"}
+                `)
+}
+else if(args.r){
+    console.log(`
+    Rules for Rock Paper Scissors:
+        - Scissors CUTS Paper
+        - Paper COVERS Rock
+        - Rock CRUSHES Scissors
+    `)
+}
+else {
+    console.log(args)
+    console.log(rps(args))
+}
+
 // rps(args)
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -2,4 +2,40 @@
 import { rpsls } from "../lib/rpsls.js"
 import minimist  from "minimist";
 const args = minimist(process.argv.slice(2));
-rpsls(args);
+
+if(args.h){
+    console.log(`
+    Usage: node-rps [SHOT] 
+    Play Rock Paper Scissors (RPS)
+-h, --help      display this help message and exit
+-r, --rules     display the rules and exit
+
+Examples:
+node-rps        Return JSON with single player RPS result.
+                e.g. {"player":"rock"}
+node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                e.g {"player":"rock","opponent":"scissors","result":"win"}
+                `)
+}
+
+if(args.r){
+    console.log(`
+    Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
+
+        - Scissors CUTS Paper
+        - Paper COVERS Rock
+        - Rock SMOOSHES Lizard
+        - Lizard POISONS Spock
+        - Spock SMASHES Scissors
+        - Scissors DECAPITATES Lizard
+        - Lizard EATS Paper
+        - Paper DISPROVES Spock
+        - Spock VAPORIZES Rock
+        - Rock CRUSHES Scissors
+    `
+    )
+}
+else {
+    console.log(rpsls(args));
+
+}
